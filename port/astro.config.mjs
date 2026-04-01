74f327b04078e7610f755c9a3e0b1a62509241f9
SID AG <agsid@gmail.com>
2026-04-01 17:19:17 -0400

    yo

--- port/astro.config.mjs
+++ port/astro.config.mjs
@@ -5,10 +5,12 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site:'https://agsid.alliedalgos.org',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
 
   vite: {
     plugins: [tailwindcss()]
